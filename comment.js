e4ecff61d012318496f736ffd520a196fddf2317
kjyhu <105508044+kjyhu@users.noreply.github.com>
2023-02-06 21:21:47 -0500

    Create comment.js

---
+++ comment.js
@@ -0,0 +1,9 @@
+/**1. The different programs communicate with each to make changes in the backend of my music app because they can modify the app
+ * without actually having to open the file itself instead, you can retrive the link on backend and make the changes on the backend.
+ * Get is the basic of the page where it is what the user see on the page. Post adds a new genre to the genres array and is being displayed
+ * throguh get. Put updates a genre that's in the genres array based on the id. Delete deletes a genre in the genres array based on the id
+ * and returns an error if the id doesn't exist.
+ * 2. I've learned that you can edit the app from the backend instead of actually editting the document and saving it and reuploading it
+ * which is a little more more time consuming.
+ * 3. This project could be further extended by requiring more other HTTP requests or other backend services.
+ */
